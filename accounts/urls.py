84f6b3210c9f63d273740c63d15a4f55c4849680
Itai Gall <itai.gall972@gmail.com>
2022-06-22 15:33:38 +0300

    urls adjusted

--- accounts/urls.py
+++ accounts/urls.py
@@ -2,7 +2,7 @@ from django.urls import path, include
 from .views import LoginView, LogoutView, UserViewSet
 
 urlpatterns = [
-    path('', include('rest_framework.urls')),
+    path(r'^', include('rest_framework.urls')),
     #POST credentials and login
     path('login/', LoginView.as_view()),
     #GET request to logout
