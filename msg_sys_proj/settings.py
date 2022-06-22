20399a51024dc1ffdaaf406a5d9c2fdcd7ea6b0f
Itai Gall <itai.gall972@gmail.com>
2022-06-22 11:19:42 +0300

    allowed hosts config

--- msg_sys_proj/settings.py
+++ msg_sys_proj/settings.py
@@ -16,7 +16,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', cast=bool)
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'https://msg-sys-ig.herokuapp.com/']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'https://msg-sys-ig.herokuapp.com']
 
 
 # Application definition
